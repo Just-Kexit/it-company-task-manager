3dc2030094f992326abafb0162916ca96e1bcb72
Bohdan Mykyta <justkexitmail@gmail.com>
2026-01-31 14:35:46 +0200

    Add app urls include and index view with default test title

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from task_manager.models import Worker, Task
+
+
+def index(request):
+
+    num_workers = Worker.objects.count()
+    num_task = Task.objects.count()
+    num_task_in_process = Task.objects.filter(is_completed=False).count()
+    num_task_is_done =  Task.objects.filter(is_completed=False).count()
+
+    context = {
+        "num_workers": num_workers,
+        "num_task": num_task,
+        "num_task_in_process": num_task_in_process,
+        "num_task_is_done": num_task_is_done
+    }
+    return HttpResponse(f"<h1>IT task manager</h1> <p>Num Worker: {context["num_workers"]} </p>")
