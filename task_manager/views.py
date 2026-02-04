1ac225e1b357c14db98b9cf127c78f1a8fae3d35
Bohdan Mykyta <justkexitmail@gmail.com>
2026-02-05 00:36:37 +0200

    Add overdue tasks to index context and template

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
@@ -12,11 +14,13 @@ def index(request):
     num_workers = Worker.objects.count()
     num_task = Task.objects.count()
     num_task_completed = Task.objects.filter(is_completed=True).count()
+    deadline_over = Task.objects.filter(deadline__lt=datetime.datetime.now())
 
     context = {
         "num_workers": num_workers,
         "num_task": num_task,
         "num_task_completed": num_task_completed,
+        "deadline_over": deadline_over
     }
     return render(request, "task_manager/index.html", context=context)
 
